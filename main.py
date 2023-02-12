28ea4dd99b4f0e114365f37eb2a43fd09a420b33
Max Bergman <max.bergman@hotmail.com>
2023-02-12 10:48:37 +0100

    Snake game with scoreboard

---
+++ main.py
@@ -0,0 +1,53 @@
+from turtle import Screen
+from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
+import time
+
+screen = Screen()
+screen.setup(width=700, height=700)
+screen.bgpic("gras.png")
+screen.title("Snake game")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.09)
+    scoreboard.speed()
+    snake.move()
+
+    # Detect food collision
+    if snake.head.distance(food) < 20:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+
+    # Detect collision with wall
+    if snake.head.xcor() > 350 or snake.head.xcor() < -350:
+        game_is_on = False
+        scoreboard.game_over()
+    # Detect collision with wall
+    if snake.head.ycor() > 350 or snake.head.ycor() < -350:
+        game_is_on = False
+        scoreboard.game_over()
+
+    # Detect collision with tail
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
+
+screen.exitonclick()
